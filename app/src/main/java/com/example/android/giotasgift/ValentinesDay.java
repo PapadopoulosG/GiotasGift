4c585856df16ba8150d63ab34441732bd389057f
linkin2895 <linkin2895@gmail.com>
2017-02-17 12:28:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/giotasgift/ValentinesDay.java
@@ -0,0 +1,13 @@
+package com.example.android.giotasgift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ValentinesDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valentines_day);
+    }
+}
